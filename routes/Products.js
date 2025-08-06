522ca577807b5994d64025846b6c0adf3873a949
Abhishek N <111786351+Abhishekn1234@users.noreply.github.com>
2025-08-06 20:16:29 +0530

    Update Products.js

--- routes/Products.js
+++ routes/Products.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const {
-  getAllProducts, createProduct, updateProduct, deleteProduct,
+  getAllProducts, createProduct, updateProduct, deleteProduct,getProductStats
 } = require('./Product');
 const { protect, admin } = require('../auth');
 const upload = require('./file');
@@ -9,5 +9,6 @@ router.get('/', getAllProducts);
 router.post('/', protect, admin, upload.single('image'),  createProduct);
 router.put('/:id', protect, admin, upload.single('image'),  updateProduct);
 router.delete('/:id', protect, admin, deleteProduct);
+router.get('/stats', protect, admin, getProductStats);
 
 module.exports = router;
